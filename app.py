0eaa7430140a3a8617ee6eb5347c3018856b8730
Sumit Patel <sumitz1207@gmail.com>
2020-12-03 17:55:13 -0500

    changed app routes to quotes, author, tags, etc

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, jsonify
 
 
 #setup engine from hawaii sqlite in resources folder
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+#engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 #reflect using automap base
 Base = automap_base()
@@ -19,88 +19,58 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #reference the two tables in Base
-Measurement = Base.classes.measurement
-Station = Base.classes.station
+#Measurement = Base.classes.measurement
+#Station = Base.classes.station
 
 
 #setup flask app
 app = Flask(__name__)
 
 
-
 # Flask Routes and welcome text
 @app.route("/")
 def welcome():
     return (
-        f"Welcome to Climate Analysis Home<br/>"
+        f"Welcome to the Quotes Scraper App<br/>"
         f"The List of Available Routes Are:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/temp/start/end<br/>"
+        f"/api/v1.0/quotes<br/>"
+        f"/api/v1.0/authors<br/>"
+        f"/api/v1.0/authors/author_name<br/>"
+        f"/api/v1.0/tags<br/>"
+        f"/api/v1.0/tags/<tag><br/>"
+        f"/api/v1.0/top10tags<br/>"
     )
 
 
-@app.route("/api/v1.0/precipitation")
-def precipitation():
-    #precipitation data of last year
-    last = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    session = Session(engine)
-
-    # Query for the date and precipitation for the last year
-    rain_list = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last).all()
-    session.close()
-
-    #create dictionary of rain and return
-    rain_dict = {date: prcp for date, prcp in rain_list}
-    return jsonify(rain_dict)
+@app.route("/api/v1.0/quotes")
+def quotes():
+    return
 
 
-@app.route("/api/v1.0/stations")
-def stations():
-    session = Session(engine)
-    station_query = session.query(Station.station).all()
-    session.close()
-    #change station query to list and return
-    station_list = list(np.ravel(station_query))
-    return jsonify(stations=station_list)
+@app.route("/api/v1.0/authors")
+def authors():
+    return
 
 
-@app.route("/api/v1.0/tobs")
-def temperatures():
-    #calculate temperature observations of last year
-    last = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    session = Session(engine)
-
-    #query of all the tobs from the last year
-    tobs_query = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= last).all()
-    session.close()
-
-    #convert query object to list
-    total_tobs = list(np.ravel(tobs_query))
-
-    # results after conversion
-    return jsonify(temps=total_tobs)
+@app.route("/api/v1.0/authors/<author_name>")
+def authorsname(author_name):
+    return
 
 
-@app.route("/api/v1.0/temp/<start>")
-def starter(start):
-    session = Session(engine)
-    #create query given start date only and calculate the min max and avg
-    temp_list1 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
-    session.close()
-    final_list1 = list(np.ravel(temp_list1))
-    return jsonify(final_list1)
+@app.route("/api/v1.0/tags")
+def tags():
+    return
 
-@app.route("/api/v1.0/temp/<start>/<end>")
-def startend(start, end):
-    session = Session(engine)
-    #create query given start date and end date as range and calculate the min max and avg
 
-    temp_list2=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-    session.close()
-    final_list2 = list(np.ravel(temp_list2))
-    return jsonify(final_list2)
+@app.route("/api/v1.0/tags/<tag>")
+def tag(tag):
+    return
+
+
+@app.route("/api/v1.0/top10tags")
+def top10tags():
+    return
+
 
 #run app
 if __name__ == '__main__':
